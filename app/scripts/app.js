517d3c2061ddda448144d63476ce7ffc147831f8
Pablo <pawel.stolka@gmail.com>
2017-10-03 20:40:35 +0200

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name authFromScratchApp
+ * @description
+ * # authFromScratchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('authFromScratchApp', []);
