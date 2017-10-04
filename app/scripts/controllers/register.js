3ab44e9bf283cf91d59880d458c37f6237a8db56
Pablo <pawel.stolka@gmail.com>
2017-10-04 05:45:28 +0200

    Add register controller

---
+++ app/scripts/controllers/register.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name authFromScratchApp.controller:RegisterCtrl
+ * @description
+ * # RegisterCtrl
+ * Controller of the authFromScratchApp
+ */
+angular.module('authFromScratchApp')
+  .controller('RegisterCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
